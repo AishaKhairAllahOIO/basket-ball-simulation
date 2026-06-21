d462f46c8df9e56c708bff4f6ff22447d4e0dbd1
AishaKhairAllahOIO <aishakhairallah3@gmail.com>
2026-06-21 19:32:15 +0300

    Edit Light

--- src/rendering/scene/createLights.js
+++ src/rendering/scene/createLights.js
@@ -1,21 +1,25 @@
 import * as THREE from "three";
 
 export function createLights(scene) {
-  const hemisphere = new THREE.HemisphereLight(0xbfdfff, 0x224422, 1.7);
+  const hemisphere = new THREE.HemisphereLight(0xbfdfff, 0x405040, 1.6);
 
-  const sun = new THREE.DirectionalLight(0xffffff, 2.2);
-  sun.position.set(-4, 7, 5);
+  const sun = new THREE.DirectionalLight(0xffffff, 1.35);
+  sun.position.set(-8, 12, 8);
   sun.castShadow = true;
 
   sun.shadow.mapSize.width = 2048;
   sun.shadow.mapSize.height = 2048;
 
   sun.shadow.camera.near = 0.5;
-  sun.shadow.camera.far = 30;
-  sun.shadow.camera.left = -10;
-  sun.shadow.camera.right = 10;
-  sun.shadow.camera.top = 10;
-  sun.shadow.camera.bottom = -10;
+  sun.shadow.camera.far = 45;
+  sun.shadow.camera.left = -24;
+  sun.shadow.camera.right = 24;
+  sun.shadow.camera.top = 20;
+  sun.shadow.camera.bottom = -20;
+
+  sun.shadow.bias = -0.00005;
+  sun.shadow.normalBias = 0.035;
+  sun.shadow.radius = 5;
 
   scene.add(hemisphere, sun);
 }
